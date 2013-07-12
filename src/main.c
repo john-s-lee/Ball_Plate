354519b121133adeb2b61cc2ebaa373aa90cc22c
john_lee___ <johnslee82@gmail.com>
2013-07-12 19:59:45 +1000

    main.c error checking

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "../include/playsound.h"
 #include "../include/manual_mode.h"
 #include "../include/ball_plate.h"
-#include "../include/wiimote_stuff.h
+#include "../include/wiimote_stuff.h"
 
 #define DOWN_BUTTON 0x0100
 #define UP_BUTTON 0x0200
